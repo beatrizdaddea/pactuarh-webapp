e112627f9453fcfc59971ff485912e607866f58a
Giovanna Linda <71105672+giovannalinda@users.noreply.github.com>
2022-02-14 14:17:13 -0300

    Structuring the files (#2)
    * adding pull request template and config for tests
    
    * adding CI
    
    * adding theme in the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import { SignIn } from 'pages'
+import { SIGN_IN } from 'routes'
+import { GlobalStyle } from 'ui'
+
 function App() {
-  return <h1>Hello World! :D</h1>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={SIGN_IN} element={<SignIn />} />
+      </Routes>
+      <GlobalStyle />
+    </BrowserRouter>
+  )
 }
 
 export default App
---
+++ src/config/theme.ts
@@ -0,0 +1,21 @@
+export const theme = {
+  colors: {
+    white: '#FFFFFF',
+    black: '#000000',
+    red: {
+      700: '#E20912',
+    },
+    neutral: {
+      100: '#F7F7F7',
+      300: '#383837',
+    },
+  },
+  font: {
+    family: 'Poppins',
+    weigths: {
+      regular: 400,
+      medium: 500,
+      bold: 700,
+    },
+  },
+}
